9ae240d306a86dbefd5e7b75df5ff05df491d601
Muhammad Imamul Azmi <imamul.azmi48@gmail.com>
2020-06-10 00:45:18 +0800

    Fix Folder Typo

--- src/ChartsServiceProvider.php
+++ src/ChartsServiceProvider.php
@@ -17,7 +17,7 @@ class ChartsServiceProvider extends ServiceProvider
     public function register(): void
     {
         // Merge the configuration files.
-        $this->mergeConfigFrom(__DIR__.'/config/charts.php', 'charts');
+        $this->mergeConfigFrom(__DIR__.'/Config/charts.php', 'charts');
         // Register the Chart Registerer singleton class to avoid resolving it
         // multiple times in the application.
         $this->app->singleton(Registrar::class, fn ($app) => new Registrar(
@@ -32,7 +32,7 @@ class ChartsServiceProvider extends ServiceProvider
     public function boot(Repository $config, Registrar $charts): void
     {
         // Publish the configuration file to the config path.
-        $this->publishes([__DIR__.'/config/charts.php' => config_path('charts.php')]);
+        $this->publishes([__DIR__.'/Config/charts.php' => config_path('charts.php')]);
         // Create the blade directrives
         $routeNamePrefix = $config->get('charts.global_route_name_prefix');
         Blade::directive('chart', function ($expression) use ($routeNamePrefix) {
